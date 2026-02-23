f4af7e127e1c69e7a74372766973a9a00272d8ad
jawahargovindasamy <jawahargovindasamy19@gmail.com>
2026-02-23 16:08:09 +0530

    fix(schedule): update cron job timing to run at 12:15 AM

--- jobs/scheduleJob.js
+++ jobs/scheduleJob.js
@@ -3,7 +3,7 @@ import { syncTodaySchedule } from "../services/scheduleService.js";
 
 export const startScheduleJob = () => {
   // Run daily at 1 AM
-  cron.schedule("25 10 * * *", async () => {
+  cron.schedule("15 0 * * *", async () => {
     console.log("📅 Running schedule sync job...");
     await syncTodaySchedule();
   },{
